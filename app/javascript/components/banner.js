283506201d488fd303bc85c60283caf1defcaebe
Celine Chambre <chambre.celine@gmail.com>
2020-11-11 13:39:31 +0100

    Add dotenv - Protect my secret data in .env file

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Create your own cocktail", "Find cocktail receipe"],
+    strings: ["Create your own cocktail", "Find cocktail recipe"],
     typeSpeed: 60,
     loop: true
   });
